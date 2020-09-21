dc6e2a7e6157621b157b00adca6ef6ec16ebb419
pupil <pupil@ivkhk.ee>
2020-09-21 14:28:03 +0300

    Готовая программа, +выводит второе слово

--- src/jktvr19_string/App.java
+++ src/jktvr19_string/App.java
@@ -5,6 +5,9 @@
  */
 package jktvr19_string;
 
+import java.util.Scanner;
+import sun.security.util.Length;
+
 /**
  *
  * @author pupil
@@ -13,6 +16,30 @@ class App {
 
     public void run() {
         System.out.println("Перевертыш");
+        System.out.println("Введите строку: ");
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine();
+        System.out.println("В строке символов: " + str.length());
+        System.out.println("Перевертыш: ");
+        char[] chStr = str.toCharArray();
+        char[] pChStr = new char[str.length()];
+        for (int i = 0; i < pChStr.length; i++) {
+            pChStr[i] = chStr[(pChStr.length-1) - i];            
+        }
+        String pStr = new String(pChStr);
+        System.out.println(pStr);
+        
+        System.out.println("Второе слово строки: ");
+        int firstSpace = str.indexOf(" ");
+        int secondSpace = str.indexOf(" ", firstSpace+1);
+        if (firstSpace < 0) System.out.println("Нет второго слова");
+        else if (secondSpace < 0) 
+            System.out.println(str.substring(firstSpace+1));
+        else
+            System.out.println(str.substring(firstSpace+1,secondSpace));
+        
+        
+        
     }
     
 }
